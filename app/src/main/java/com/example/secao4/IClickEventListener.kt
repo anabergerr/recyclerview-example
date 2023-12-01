f5e331b60bc1d6afcae4532bfb35e559a78847b3
anabergerr <ana.berger@betrybe.com>
2023-12-01 00:55:59 -0300

    adicionando evento de click :)))

---
+++ app/src/main/java/com/example/secao4/IClickEventListener.kt
@@ -0,0 +1,5 @@
+package com.example.secao4
+
+interface IClickEventListener {
+    fun onItemClick(position: Int): Unit;
+}
--- app/src/main/java/com/example/secao4/MainActivity.kt
+++ app/src/main/java/com/example/secao4/MainActivity.kt
@@ -5,15 +5,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ExpandableListView.OnChildClickListener
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.text.FieldPosition
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), IClickEventListener {
+
+    data class Movie(val title: String, val director: String)
+
+    private val movieList = listOf(
+        Movie("Alien", "Ridley Scott"),
+        Movie("La La Land", "Damien Chazelle"),
+        Movie("Nomadland", "Chloe Zhao"),
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
 
@@ -21,29 +33,33 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
 
 
-        data class Movie(val title: String, val director: String)
+        class MovieViewHolder(itemView: View, private val clickListener: IClickEventListener) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
 
-        val movieList = listOf(
-            Movie("Alien", "Ridley Scott"),
-            Movie("La La Land", "Damien Chazelle"),
-            Movie("Nomadland", "Chloe Zhao"),
-        )
+            init {
+                itemView.setOnClickListener(this)
+            }
+
+            override fun onClick(p0: View?){
 
 
+                val position = adapterPosition
 
-        class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+                if(position != RecyclerView.NO_POSITION){
+                    clickListener.onItemClick(position)
+                }
+            }
             fun bind(movie: Movie) {
                 itemView.findViewById<TextView>(R.id.movieTitle).text = movie.title
                 itemView.findViewById<TextView>(R.id.movieDirector).text = movie.director
             }
         }
 
-        class MovieAdapter(private val movies: List<Movie>) :
+        class MovieAdapter(private val movies: List<Movie>, private val clickListener: IClickEventListener) :
             RecyclerView.Adapter<MovieViewHolder>() {
             override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
                 val view =
                     LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
-                return MovieViewHolder(view)
+                return MovieViewHolder(view, clickListener)
             }
 
             override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
@@ -57,10 +73,13 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        val adapter = MovieAdapter(movieList)
+        val adapter = MovieAdapter(movieList, this)
 
         recyclerView.adapter = adapter
-
+    }
+    override fun onItemClick(position: Int) {
+        val movie = movieList[position]
+        Toast.makeText(this, movie.title, Toast.LENGTH_SHORT).show()
 
     }
 }
